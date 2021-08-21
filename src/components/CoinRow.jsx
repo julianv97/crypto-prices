557559274f1eca6d52d275303b8e4e807a9ca088
Julian Vicente <julian.vicente97s@gmail.com>
2021-08-21 18:01:05 -0300

    table

--- src/components/CoinRow.jsx
+++ src/components/CoinRow.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useContext } from "react";
+import CoinsContext from "../context/CoinsContext";
 
-const CoinRow = () => {
-    return (
-        <div>
-            
+const CoinRow = ({ coin }) => {
+  const { state } = useContext(CoinsContext);
+  return (
+    <tr key={coin.name}>
+      <td className="px-6 py-4">
+        <div className="flex items-center">
+          <div className="flex-shrink-0 h-8 w-8">
+            <img className="h-8 w-8 rounded-full" src={coin.image} alt="" />
+          </div>
+          <div className="ml-4 flex ">
+            <div className="text-sm font-medium text-gray-900">{coin.name}</div>
+            <div className="text-sm text-gray-500">{coin.symbol}</div>
+          </div>
         </div>
-    )
-}
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-900">
+          {state.currency === "ARS"
+            ? ((coin.current_price / 96.5) * 179).toFixed(2)
+            : coin.current_price.toFixed(2)}
+        </div>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+          Active
+        </span>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+        {coin.name}
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+        {coin.name}
+      </td>
+    </tr>
+  );
+};
 
-export default CoinRow
+export default CoinRow;
--- src/components/TableCoins.jsx
+++ src/components/TableCoins.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useContext, useEffect } from "react";
 import CoinsContext from "../context/CoinsContext";
+import CoinRow from "./CoinRow";
 
 const currencies = ["USD", "EUR", "ARS", "CNY"];
+const titles = ["Coin", "Price", "Price Change", "24h Volume", "Market-Cap"];
 
 const TableCoins = () => {
   const { state, handleDispatch } = useContext(CoinsContext);
@@ -18,43 +20,67 @@ const TableCoins = () => {
 
   return (
     <div>
-      <h1>Search a coin</h1>
-      <form action="" onSubmit={(e) => handleDispatch("PREVENT_DEFAULT", e)}>
-        <input
-          type="text"
-          name=""
-          id=""
-          className="bg-red-300"
-          onChange={(e) => setSearchCoin(e.target.value)}
-        />
-        <select
-          onChange={(e) => {
-            handleDispatch("CHANGE_CURRENCY", e.target.value);
-          }}
+      <form
+        action=""
+        onSubmit={(e) => handleDispatch("PREVENT_DEFAULT", e)}
+        className="w-96 flex"
+      >
+        <label
+          htmlFor="price"
+          className="block text-sm font-medium text-gray-700"
         >
-          {currencies.map((currency) => (
-            <option value={currency} key={currency} >
-              {currency}
-            </option>
-          ))}
-        </select>
-      </form>
-      {filteredCoins.map((coin) => {
-        return (
-          <div key={coin.name} className="flex">
-            <img src={coin.image} alt={coin.name} className="w-8 h-8"></img>
-            <div>
-            <span>{coin.name}</span>
-            <span>
-              {state.currency === "ARS"
-                ? ((coin.current_price / 96.5) * 179).toFixed(2)
-                : coin.current_price.toFixed(2)}
-            </span>
-            </div>
+          Search a coin:
+        </label>
+        <div className="mt-1 relative rounded-md shadow-sm">
+          <input
+            type="text"
+            name="price"
+            id="price"
+            className="focus:ring-indigo-500 focus:border-indigo-500 block pl-7 pr-12 border-gray-300 rounded-md w-full"
+            onChange={(e) => setSearchCoin(e.target.value)}
+          />
+          <div className="absolute inset-y-0 right-0 flex items-center">
+            <label htmlFor="currency" className="sr-only">
+              Currency
+            </label>
+            <select
+              id="currency"
+              name="currency"
+              className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500  rounded-md"
+              onChange={(e) => {
+                handleDispatch("CHANGE_CURRENCY", e.target.value);
+              }}
+            >
+              {currencies.map((currency) => (
+                <option value={currency} key={currency}>
+                  {currency}
+                </option>
+              ))}
+            </select>
           </div>
-        );
-        //CARTEL CUANDO NO ENCUENTRA LA MONEDA
-      })}
+        </div>
+      </form>
+
+      <table className="min-w-full divide-y divide-gray-200 table-fixed">
+        <thead className="">
+          <tr className="text-sm sticky top-0 bg-gray-50  ">
+            {titles.map((title, index) => (
+              <th
+                key={index}
+                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase "
+              >
+                {title}
+              </th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody className="bg-white divide-y divide-gray-200">
+          {filteredCoins.map((coin) => (
+            <CoinRow coin={coin} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
