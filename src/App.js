20fd8d622825a2e9a6d633fbb2e41152677995be
Julian Vicente <julian.vicente97s@gmail.com>
2021-08-22 01:59:40 -0300

    lazy load

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React,{Suspense, lazy} from "react";
 import Header from "./components/Header";
-import TableCoins from "./components/TableCoins";
+/* import TableCoins from "./components/TableCoins"; */
 
 import CoinsState from "./context/CoinsState";
+const TableCoins = lazy(()=>import('./components/TableCoins'))
+
 
 function App() {
   return (
     <CoinsState>
       <div className="dark:bg-gray-800 w-full h-full min-h-screen transition duration-500 ">
         <Header />
+        <Suspense fallback={<h1>Loading...</h1>}>
         <TableCoins />
+        </Suspense>
       </div>
     </CoinsState>
   );
